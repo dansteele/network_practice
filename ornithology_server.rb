c5226c083c55d3ca62b2c60012f89abf60d88129
dansteele <danielsteele@hotmail.co.uk>
2015-02-08 21:16:31 +0000

    Completed

--- ornithology_server.rb
+++ ornithology_server.rb
@@ -20,25 +20,43 @@ set :birds, {}
 set :partial_template_engine, :erb
 enable :partial_underscores
 
+def my_amazing_layout
+  {:layout => :my_amazing_layout, :layout_options => { :views => 'views/layouts' }}
+end
+
 get "/" do
   @bird = Bird.bird_of_the_day
-  erb :homepage,
-  :layout => :my_amazing_layout, :layout_options => { :views => 'views/layouts' }
+  erb :homepage, my_amazing_layout
 end
 
 get '/birds/:bird' do
   @bird = Bird.find_by_name(params[:bird])
-  erb :bird_page,
-  :layout => :my_amazing_layout, :layout_options => { :views => 'views/layouts' }
+  erb :bird_page, my_amazing_layout
 end
 
 get '/new' do
-  erb :bird_form,
-  :layout => :my_amazing_layout, :layout_options => { :views => 'views/layouts' }
+  erb :bird_form, my_amazing_layout
+end
+
+get '/edit/:id' do
+  @bird = Bird.find(params[:id])
+  erb :edit_page, my_amazing_layout
 end
 
 post '/create' do
   bird = Bird.new(params)
   bird.save
   redirect "birds/#{bird.name}"
+end
+
+post '/update/:id' do
+  @bird = Bird.find(params[:id])
+  Bird.update(params)
+
+  redirect "birds/#{bird.name}"
+end
+
+post '/delete/:id' do
+  Bird.find(params[:id]).destroy
+  redirect '/'
 end
